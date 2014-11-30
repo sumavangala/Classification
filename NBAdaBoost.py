e6703b0c3d1fc8487eca192e2aac21f54b2627cc
Suma <suma.reddy871@gmail.com>
2014-11-30 05:22:37 -0600

    NB with Adaboost updated

--- NBAdaBoost.py
+++ NBAdaBoost.py
@@ -2,6 +2,7 @@ __author__ = 'suma'
 
 
 import numpy
+import math
 
 
 def main():
@@ -10,14 +11,13 @@ def main():
     totalAttributes = 0
     totalRecords = 0
     attributes = []  #1 to totalAttributes
-    weights = [dict() for x in range(k+1)]
-    sampleSet = [dict() for x in range(k+1)]
+    weights = {}
+    sampleSet = {}
     totalPositiveClassRecords = {}
     totalNegativeClassRecords = {}
     positiveClassDict = [dict() for x in range(k)]
     negativeClassDict = [dict() for x in range(k)]
-    # actualLabels = [dict() for x in range(k)]
-    # predictedLabels = [dict() for x in range(k)]
+    errorRate = {}
 
     #Find the total number of attributes - max from train and test file
     trainFile = open(train_file_path, 'r')
@@ -50,16 +50,12 @@ def main():
         attributes.append(x)
 
     for x in range(1, totalRecords + 1, 1):
-        weights[0][x] = 1*1.0/totalRecords
-        sampleSet[0][x] = x
+        weights[x] = 1*1.0/totalRecords
+        sampleSet[x] = x
 
     #run the classifier k rounds
     for counter in range(1, k+1, 1):
-        sample = numpy.random.choice(sampleSet[counter-1].values(), totalRecords, p=weights[counter-1].values())
-        index = 1
-        for x in sample:
-            sampleSet[counter][index] = x
-            index += 1
+        sample = numpy.random.choice(sampleSet.values(), totalRecords, 1, p=weights.values())
 
         for each in sample:
             line = trainFileContent[each]
@@ -161,14 +157,198 @@ def main():
                 else:
                     predictedLabels[index] = '-1'
 
-        break
-    # print totalNegativeClassRecords
-    # print totalPositiveClassRecords
-    # print positiveClassDict
-    # print negativeClassDict
+        #Calculating the weighted error rate
+        error = 0
+        newWeightSum = 0
+        for key in actualLabels:
+            actualClass = actualLabels[key]
+            predictedClass = predictedLabels[key]
+            if (actualClass == '+1' and predictedClass == '-1') or (actualClass == '-1' and predictedClass == '+1'):
+                error = error + weights[key]
+        errorRate[counter] = error
 
+        #Adjusting the weights of a tuple if correctly classified
+        for key in actualLabels:
+            actualClass = actualLabels[key]
+            predictedClass = predictedLabels[key]
+            if (actualClass == '+1' and predictedClass == '+1') or (actualClass == '-1' and predictedClass == '-1'):
+                weights[key] = weights[key] * ((error*1.0)/(1-error))
+            newWeightSum = newWeightSum + weights[key]
+
+        #normalize the weights
+        for key in actualLabels:
+            weights[key] = weights[key] * 1.0/newWeightSum
+
+    #Predict labels for training data and print FP,TP,TN,FN
+    index = 0
+    actualLabels = {}
+    predictedLabels = {}
+
+    for each in trainFileContent:
+        line = trainFileContent[each].strip()
+        if len(line) > 0:
+            index = index + 1
+            record = line.split()
+            actualLabels[index] = record[0]
+            record.pop(0)
+
+            #Combine k classifiers to classify the dataset
+            for counter in range(1, k+1, 1):
+                result = 0
+
+                #probability of +1
+                Pr1 = (totalPositiveClassRecords[counter]*1.0)/totalRecords
+                #propability of -1
+                Pr2 = (totalNegativeClassRecords[counter]*1.0)/totalRecords
+
+                positiveTotalProb = Pr1
+                negativeTotalProb = Pr2
+                attrList = []
+                for r in record:
+                    pair = r.split(':')
+                    attrList.append(int(pair[0]))
+
+                    if(positiveClassDict[counter-1].has_key(r)):
+                        positiveTotalProb = positiveTotalProb * ((positiveClassDict[counter-1][r]*1.0)/totalPositiveClassRecords[counter])
+                    else:
+                        positiveTotalProb = positiveTotalProb * 0
+
+                    if(negativeClassDict[counter-1].has_key(r)):
+                        negativeTotalProb = negativeTotalProb * ((negativeClassDict[counter-1][r]*1.0)/totalNegativeClassRecords[counter])
+                    else:
+                        negativeTotalProb = negativeTotalProb * 0
+
+                diff = set(attributes) - set(attrList)  #attributes for which values are 0
+                for x in diff:
+                    key = str(x) + ':0'
+                    if(positiveClassDict[counter-1].has_key(key)):
+                        positiveTotalProb = positiveTotalProb * ((positiveClassDict[counter-1][key]*1.0)/totalPositiveClassRecords[counter])
+                    else:
+                        positiveTotalProb = positiveTotalProb * 0
+
+                    if(negativeClassDict[counter-1].has_key(key)):
+                        negativeTotalProb = negativeTotalProb * ((negativeClassDict[counter-1][key]*1.0)/totalNegativeClassRecords[counter])
+                    else:
+                        negativeTotalProb = negativeTotalProb * 0
+
+                if positiveTotalProb > negativeTotalProb:
+                    result = result + math.log((1-errorRate[counter])*1.0/errorRate[counter])
+                else:
+                    result = result - math.log((1-errorRate[counter])*1.0/errorRate[counter])
+
+            if result > 0:
+                predictedLabels[index] = '+1'
+            else:
+                predictedLabels[index] = '-1'
+
+
+    #Calcualate TP, FN, FP, TN for training dataset
+    truePositives = 0
+    falseNegatives = 0
+    falsePositives = 0
+    trueNegatives = 0
+
+    for key in actualLabels:
+        actualClass = actualLabels[key]
+        predictedClass = predictedLabels[key]
+        if(actualClass == '+1' and predictedClass == '+1'):
+            truePositives += 1
+        elif(actualClass == '+1' and predictedClass == '-1'):
+            falseNegatives += 1
+        elif(actualClass == '-1' and predictedClass == '+1'):
+            falsePositives += 1
+        elif(actualClass == '-1' and predictedClass == '-1'):
+            trueNegatives += 1
+
+    print "%d %d %d %d" % (truePositives, falseNegatives, falsePositives, trueNegatives)
+    print ((truePositives + trueNegatives)*100.0) / (truePositives+trueNegatives+falsePositives+falseNegatives)
+
+    #Predict labels for test data and print FP,TP,TN,FN
+    index = 0
+    actualLabels = {}
+    predictedLabels = {}
+
+    testFile = open(test_file_path, 'r')
+    for line in testFile:
+        line = line.strip()
+        if len(line) > 0:
+            index = index + 1
+            record = line.split()
+            actualLabels[index] = record[0]
+            record.pop(0)
+
+            #Combine k classifiers to classify the dataset
+            for counter in range(1, k+1, 1):
+                result = 0
+
+                #probability of +1
+                Pr1 = (totalPositiveClassRecords[counter]*1.0)/totalRecords
+                #propability of -1
+                Pr2 = (totalNegativeClassRecords[counter]*1.0)/totalRecords
+
+                positiveTotalProb = Pr1
+                negativeTotalProb = Pr2
+                attrList = []
+                for r in record:
+                    pair = r.split(':')
+                    attrList.append(int(pair[0]))
+
+                    if(positiveClassDict[counter-1].has_key(r)):
+                        positiveTotalProb = positiveTotalProb * ((positiveClassDict[counter-1][r]*1.0)/totalPositiveClassRecords[counter])
+                    else:
+                        positiveTotalProb = positiveTotalProb * 0
+
+                    if(negativeClassDict[counter-1].has_key(r)):
+                        negativeTotalProb = negativeTotalProb * ((negativeClassDict[counter-1][r]*1.0)/totalNegativeClassRecords[counter])
+                    else:
+                        negativeTotalProb = negativeTotalProb * 0
+
+                diff = set(attributes) - set(attrList)  #attributes for which values are 0
+                for x in diff:
+                    key = str(x) + ':0'
+                    if(positiveClassDict[counter-1].has_key(key)):
+                        positiveTotalProb = positiveTotalProb * ((positiveClassDict[counter-1][key]*1.0)/totalPositiveClassRecords[counter])
+                    else:
+                        positiveTotalProb = positiveTotalProb * 0
+
+                    if(negativeClassDict[counter-1].has_key(key)):
+                        negativeTotalProb = negativeTotalProb * ((negativeClassDict[counter-1][key]*1.0)/totalNegativeClassRecords[counter])
+                    else:
+                        negativeTotalProb = negativeTotalProb * 0
+
+                if positiveTotalProb > negativeTotalProb:
+                    result = result + math.log((1-errorRate[counter])*1.0/errorRate[counter])
+                else:
+                    result = result - math.log((1-errorRate[counter])*1.0/errorRate[counter])
+
+            if result > 0:
+                predictedLabels[index] = '+1'
+            else:
+                predictedLabels[index] = '-1'
+
+
+    #Calcualate TP, FN, FP, TN for training dataset
+    truePositives = 0
+    falseNegatives = 0
+    falsePositives = 0
+    trueNegatives = 0
+
+    for key in actualLabels:
+        actualClass = actualLabels[key]
+        predictedClass = predictedLabels[key]
+        if(actualClass == '+1' and predictedClass == '+1'):
+            truePositives += 1
+        elif(actualClass == '+1' and predictedClass == '-1'):
+            falseNegatives += 1
+        elif(actualClass == '-1' and predictedClass == '+1'):
+            falsePositives += 1
+        elif(actualClass == '-1' and predictedClass == '-1'):
+            trueNegatives += 1
+
+    print "%d %d %d %d" % (truePositives, falseNegatives, falsePositives, trueNegatives)
+    print ((truePositives + trueNegatives)*100.0) / (truePositives+trueNegatives+falsePositives+falseNegatives)
 
 if __name__ == "__main__":
-    train_file_path = "/home/suma/Desktop/Data Mining/Assignment/Assignment4/dataset/adult.train"
-    test_file_path = "/home/suma/Desktop/Data Mining/Assignment/Assignment4/dataset/adult.test"
+    train_file_path = "/home/suma/Desktop/Data Mining/Assignment/Assignment4/dataset/led.train"
+    test_file_path = "/home/suma/Desktop/Data Mining/Assignment/Assignment4/dataset/led.test"
     main()
